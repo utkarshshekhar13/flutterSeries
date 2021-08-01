412f1eee44a973b23fd340048c5b54cf24913819
Utkarsh Shekhar <utki.619@icloud.com>
2021-08-01 15:13:25 +0530

    day 4 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/pages/home_page.dart';
 import 'package:flutter_catalog/pages/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -17,9 +18,13 @@ class MyApp extends StatelessWidget {
     // addNumber(4, 5);
     return MaterialApp(
       themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        fontFamily: GoogleFonts.lato().fontFamily,
+       
+        ),
       darkTheme: ThemeData(brightness: Brightness.light),
-      initialRoute: "/home",
+      // initialRoute: "/home",
       routes: {
         "/":(context) => LoginPage(),
         "/home":(context) => HomePage(), 
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,11 +1,58 @@
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({ Key? key }) : super(key: key);
+  const LoginPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Material(
+        color: Colors.white,
+        child: Column(
+          children: [
+            Image.asset("assets/images/login_image.png", fit: BoxFit.cover),
+            SizedBox(
+              height: 20.0,
+            ),
+            Text(
+              "Welcome",
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                        hintText: "Enter your username", labelText: "Username"),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        hintText: "Enter your password", labelText: "Password"),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  ElevatedButton(
+                      onPressed: () {
+                        print("Hello");
+                      },
+                      child: Text("Login"))
+                ],
+              ),
+            )
+          ],
+        ));
+  }
+}
+
+/*
+
+ return Material(
       child: Center(
         child: Text("Login Page",
         style: TextStyle(
@@ -18,5 +65,6 @@ class LoginPage extends StatelessWidget {
       ),
       
     );
-  }
-}
+
+
+ */
